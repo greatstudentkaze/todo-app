4b273a25508d83f3ebb35d87eace7dc5fadb0f2a
gsk <dfyz0r@list.ru>
2021-02-25 10:03:30 +0300

    feat: add Header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TaskSearchForm from '../task-search-form';
+
+const Header = () => {
+  return (
+    <header className="page-header">
+      <TaskSearchForm />
+    </header>
+  );
+};
+
+export default Header;
