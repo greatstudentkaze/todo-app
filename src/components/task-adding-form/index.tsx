c263ea039ffe46c53405d4da92f51e67d9f1d063
gsk <dfyz0r@list.ru>
2021-02-25 10:04:53 +0300

    feat(task-adding-form): add changes for styles and make input field controlled

--- src/components/task-adding-form/index.tsx
+++ src/components/task-adding-form/index.tsx
@@ -1,35 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { addTask } from '../../redux/actions/tasks';
 
 import { TaskItem as TaskItemType } from '../../types';
 
-const TaskAddingForm = () => {
+type Props = {
+  hide: () => void,
+}
+
+const TaskAddingForm = ({ hide }: Props) => {
   const dispatch = useDispatch();
-  const titleInputRef = useRef<HTMLInputElement>(null);
+  const [title, setTitle] = useState<string>('');
 
   const handleSubmit = (evt: React.SyntheticEvent) => {
     evt.preventDefault();
 
-    if (titleInputRef.current!.value) {
+    if (title.trim()) {
       const task: TaskItemType = {
         id: String(Date.now()),
-        title: titleInputRef.current!.value,
+        title: title.trim(),
         completed: false,
       };
 
       dispatch(addTask(task));
 
-      titleInputRef.current!.value = '';
+      setTitle('');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="task-title">Введите название задачи:</label>
-      <input ref={titleInputRef} type="text" name="title" id="task-title" placeholder="Съесть пиццу"/>
-      <button type="submit">Добавить</button>
+    <form className="task-adding-form" onSubmit={handleSubmit} autoComplete="off">
+      <div className="task-adding-form__edit-area">
+        <input
+          className="task-adding-form__input"
+          type="text"
+          name="title"
+          id="task-title"
+          placeholder="Съесть пиццу"
+          aria-label="Название задачи"
+          value={title}
+          onChange={evt => setTitle(evt.target.value)} />
+      </div>
+      <div className="task-adding-form__buttons">
+        <button className="task-adding-form__button task-adding-form__button--add button" type="submit" disabled={!title.trim()}>Добавить</button>
+        <button className="task-adding-form__button task-adding-form__button--cancel button" type="button" onClick={hide}>Отмена</button>
+      </div>
     </form>
   );
 };
