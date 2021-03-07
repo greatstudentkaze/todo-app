9e8207209713da0e71c3d35673d497bf3597289e
gsk <dfyz0r@list.ru>
2021-03-07 14:52:09 +0300

    feat(setupTests.ts): add 17 react adapter

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,4 @@
 import Enzyme from 'enzyme';
-import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import ReactSeventeenAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-Enzyme.configure({ adapter: new ReactSixteenAdapter() });
+Enzyme.configure({ adapter: new ReactSeventeenAdapter() });
